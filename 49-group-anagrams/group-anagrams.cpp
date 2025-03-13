bf4ae8296d3966685511ee00636a8dbd90369aaa
manmohan soni <manmohansoni1094@gmail.com>
2025-03-13 22:55:57 +0530

    Time: 12 ms (86.27%) | Memory: 24.9 MB (76.62%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> anagrams;
+        for (string str : strs) {
+            string sortedStr = str;
+            sort(sortedStr.begin(), sortedStr.end());
+            anagrams[sortedStr].push_back(str);
+        }
+        vector<vector<string>> result;
+        for (auto& pair : anagrams) {
+            result.push_back(pair.second);
+        }
+        return result;
+    }
+};
